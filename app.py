459e87ba9717ac693cc10d8177bc57b8c6584bb8
Aman kumar <70831554+amankr077@users.noreply.github.com>
2025-07-11 11:22:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def generate():
     return jsonify({'result': compressed})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT if available
+    app.run(host='0.0.0.0', port=port)
